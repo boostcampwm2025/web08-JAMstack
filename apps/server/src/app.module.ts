3dc463ea56c1e93eda607370dc040708ebb48f15
son-hyejun <h_jun0515@naver.com>
2025-12-16 14:05:28 +0900

    ✨ feat: NestJS 서버 모듈 구조 및 기본 아키텍처 정리 #25
    - 서버 src 하위에 common, config, modules 레이어 구성
    - auth, room, collaboration, file 도메인 모듈 생성
    - 공통/설정 디렉토리에 .gitkeep 추가
    - AppModule 구조 정리

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { RoomModule } from './modules/room/room.module';
+import { CollaborationModule } from './modules/collaboration/collaboration.module';
+import { FileModule } from './modules/file/file.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, RoomModule, CollaborationModule, FileModule],
 })
 export class AppModule {}
