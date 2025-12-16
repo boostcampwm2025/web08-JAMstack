9e1419e7b8ad3ced6b11f44761d9eb242003b40b
kindsmell <kindsmell33@gmail.com>
2025-12-16 18:12:41 +0900

    ✨ feat: 세션 응답 DTO 생성 (packages/common)
    - packages/common/src/dtos/session-response.dto.ts 생성
    - SessionResponseDto 클래스 정의 (sessionId, nickname, color, joinedAt)
    - packages/common/src/dtos/index.ts 생성 (export 설정)
    - packages/common/src/index.ts에서 dtos export 추가
    - 프론트엔드와 백엔드에서 공통 사용 가능

--- packages/common/src/dtos/index.ts
+++ packages/common/src/dtos/index.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export * from "./session-response.dto.js";
+=======
+export * from "./session-response.dto";
+>>>>>>> 2a14789 (✨ feat: 세션 응답 DTO 생성 (packages/common))
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,4 +2,8 @@ export * from './constants/index.js';
 export * from './types/index.js';
 
 export const PROJECT_NAME = 'CodeJam';
+<<<<<<< HEAD
 export * from './dtos/index.js';
+=======
+export * from './dtos';
+>>>>>>> 2a14789 (✨ feat: 세션 응답 DTO 생성 (packages/common))
