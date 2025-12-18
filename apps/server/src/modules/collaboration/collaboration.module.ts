b3661262768411313b1b82169325caecc62b2725
Happysttim <gmlsdyd1023@naver.com>
2025-12-18 14:51:09 +0900

    ✨feat: RoomModule 을 import

--- apps/server/src/modules/collaboration/collaboration.module.ts
+++ apps/server/src/modules/collaboration/collaboration.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CollaborationGateway } from './collaboration.gateway';
+import { RoomModule } from '../room/room.module';
 
 @Module({
+  imports: [RoomModule],
   providers: [CollaborationGateway],
 })
 export class CollaborationModule {}
